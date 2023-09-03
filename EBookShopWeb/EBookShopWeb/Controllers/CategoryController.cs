8b6af7e18bb74e1d0eca237a8b3bf3511c642980
harun <harun@DESKTOP-OBA4SBK>
2023-09-03 17:04:10 +0300

    CategoryList

--- EBookShopWeb/EBookShopWeb/Controllers/CategoryController.cs
+++ EBookShopWeb/EBookShopWeb/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBookShopWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EBookShopWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbcontext;
+        public CategoryController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList= _dbcontext.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
