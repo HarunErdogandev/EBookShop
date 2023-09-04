58b4de76371177fd680b64aae07f5d38a8cbfad4
harun <harun@DESKTOP-OBA4SBK>
2023-09-04 12:46:37 +0300

    W

--- EBookShopWeb/EBookShopWeb/Controllers/CategoryController.cs
+++ EBookShopWeb/EBookShopWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using EBookShopWeb.Data;
+using EBookShopWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EBookShopWeb.Controllers
@@ -15,6 +16,67 @@ namespace EBookShopWeb.Controllers
         {
             var objCategoryList= _dbcontext.Categories.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Aynı değerde olamaz");
+            }
+            if(ModelState.IsValid)
+            {
+                _dbcontext.Categories.Add(category);
+                _dbcontext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            if (id==0 || id==null)
+            {
+                return NotFound();
+            }
+            var category = _dbcontext.Categories.Find(id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Aynı değerde olamaz");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Categories.Update(category);
+                _dbcontext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            var category = _dbcontext.Categories.Find(id);
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Categories.Remove(category);
+                _dbcontext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- EBookShopWeb/EBookShopWeb/Models/Category.cs
+++ EBookShopWeb/EBookShopWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EBookShopWeb.Models
 {
@@ -6,8 +7,12 @@ namespace EBookShopWeb.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(AllowEmptyStrings =false,ErrorMessage ="Boş geçme lan")]
+        [Required(ErrorMessage ="Lütfen eklemek istediğiniz kategorinin ismini belirtin")]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
