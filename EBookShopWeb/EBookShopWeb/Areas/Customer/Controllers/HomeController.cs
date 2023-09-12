3aa45c710b267494909ae812d86cd40980bcd249
harun <harun@DESKTOP-OBA4SBK>
2023-09-12 18:16:30 +0300

    a

--- EBookShopWeb/EBookShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ EBookShopWeb/EBookShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using EBookShopWeb.Models;
+﻿using DataAccess.Repository.IRepository;
+using EBookShopWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Models.Models;
 using System.Diagnostics;
 
 namespace EBookShopWeb.Areas.Customer.Controllers
@@ -8,15 +11,25 @@ namespace EBookShopWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
+       
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
